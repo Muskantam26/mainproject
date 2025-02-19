546ff9ff7021c8547c94a226356474b40ca5ba86
Muskantam26 <mskntmrkr@gmail.com>
2025-02-20 03:08:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,92 @@
+
+VANTA.HALO({
+    el: "#ani",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00
+  })
+
+
+let signUp=() =>{
+    let name = document.querySelector("#name").value
+    let inpnum = document.querySelector("#num").value
+    let passwor = document.querySelector("#password").value
+    
+    let errname=document.querySelector("#errname")
+    let errnum=document.querySelector("#errnum")
+    let errpass=document.querySelector("#errpass")
+
+
+    if(name==""){
+        errname.innerHTML="please enter your valid name"
+        errname.style.color="red"
+        return false;
+    }
+    if(inpnum==""){
+        errnum.innerHTML="please enter your  number"
+        errnum.style.color="red"
+        return false;
+
+    }
+    else if(isNaN(inpnum)){
+        errnum.innerHTML="please enter your  number only"
+        errnum.style.color="red"
+        return false;
+
+    }
+    else if(inpnum.length!=10){
+        errnum.innerHTML="please enter your valid number"
+        errnum.style.color="red"
+        return false;
+
+    }
+    if(passwor==""){
+        errpass.innerHTML="please enter your valid pass"
+        errpass.style.color="red"
+        return false;
+
+    }
+    else if(!(passwor.match(/[1234567890]/)
+         && passwor.match(/[!@#$%&*^()]/)
+         && passwor.match(/[a-z]/)
+         && passwor.match(/[A-Z]/)))
+         {
+           errpass.innerHTML="please create strong password"
+           errnum.innerHTML=""
+             errpass.style.color="red"
+             return false
+         }
+    
+
+
+    
+    // else{
+    //     alert('enter your correct number or name')
+    // }
+    
+    localStorage.setItem("name",name)
+    localStorage.setItem("number",inpnum)
+    localStorage.setItem("password",passwor)
+
+
+    location.href="login.html"
+return false;
+}
+
+let login=()=>{
+    let loginname=document.querySelector("#namee").value
+    let loginpass=document.querySelector("#pass").value
+
+    let localname=localStorage.getItem("name")
+    localpass=localStorage.getItem("password")
+if(loginname==localname && loginpass==localpass){
+    location.href="home.html"
+}
+// else{
+//     alert("invalid username or password")
+// }
+return false;
+
+}
