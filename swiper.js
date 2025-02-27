63352be81ab5b77c4761707cd33ad93f355bd1f9
Muskantam26 <mskntmrkr@gmail.com>
2025-02-27 17:03:33 +0530

    update

--- swiper.js
+++ swiper.js
@@ -40,3 +40,23 @@ var swiper2 = new Swiper(".mySwiper1", {
     delay:2000,
   }
 });
+
+// third section
+
+
+var swiper = new Swiper(".mySwiper2", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination2",
+  },
+});
