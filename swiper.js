c7c00878b2389b9f8b39db811df70111007be951
Muskantam26 <mskntmrkr@gmail.com>
2025-02-28 14:20:12 +0530

    update

--- swiper.js
+++ swiper.js
@@ -44,19 +44,3 @@ var swiper2 = new Swiper(".mySwiper1", {
 // third section
 
 
-var swiper = new Swiper(".mySwiper2", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows: true,
-  },
-  pagination: {
-    el: ".swiper-pagination2",
-  },
-});
